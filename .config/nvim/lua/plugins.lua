dc9593b466e698d6c80d49f252698153c334b06b
seybo <michaelatworkspace@gmail.com>
2024-03-11 17:23:44 +0530

    add noice vim plug

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -420,6 +420,21 @@ local plugins = {
         config = true,
         cmd = "Glow",
     },
+    -- {
+    --     "folke/noice.nvim",
+    --     opts = {
+    --         -- add any options here
+    --     },
+    --     dependencies = {
+    --         -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+    --         "MunifTanjim/nui.nvim",
+    --         -- OPTIONAL:
+    --         --   `nvim-notify` is only needed, if you want to use the notification view.
+    --         --   If not available, we use `mini` as the fallback
+    --         "rcarriga/nvim-notify",
+    --     },
+    --     config = require("plugins.noice").setup,
+    -- },
 
     -- {
     --     "subnut/nvim-ghost.nvim",
