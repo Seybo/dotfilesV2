57a7f41b85e2b4120d6daadc01b521ef7713c354
seybo <michaelatworkspace@gmail.com>
2024-03-11 17:22:01 +0530

    fix mapping function, fix .tmux.conf.local, add .gemrc

--- .config/nvim/lua/utils/map.lua
+++ .config/nvim/lua/utils/map.lua
@@ -11,24 +11,29 @@ _G.map = function(mapping)
     local mode = mapping.mode or { "n" }
     local msg = mapping.msg
 
-    mapping["mode"] = nil
-    mapping["desc"] = desc
-
-    local final_cmd
-    if msg then
-        final_cmd = function()
-            -- Use nvim_echo to show the message
-            vim.api.nvim_echo({ { msg, "None" } }, true, {})
-            -- Schedule clearing the message after 2 seconds
-            vim.defer_fn(function() vim.cmd('echo ""') end, 2000)
-            cmd() -- Then execute the command
-        end
-    else
-        final_cmd = cmd
-    end
-
+    -- Remove 'mode' and 'msg' from mapping to prevent errors
     mapping.mode, mapping.msg = nil, nil
+
+    -- Prepare options by merging default_options with any additional options from mapping
+    -- This is done once here to avoid duplication and ensure efficiency
     local options = vim.tbl_extend("force", default_options, mapping)
 
-    vim.keymap.set(mode, key, final_cmd, options)
+    if msg then
+        local final_cmd = function()
+            print(msg)                                            -- Print the message
+            vim.defer_fn(function() vim.cmd('echo ""') end, 2000) -- Clear message after 2 seconds
+
+            if type(cmd) == "function" then
+                cmd() -- Execute the command if it's a Lua function
+            elseif type(cmd) == "string" then
+                -- If cmd is a string, simulate key presses
+                vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(cmd, true, false, true), "t", false)
+            end
+        end
+        -- Apply the final_cmd with message handling
+        vim.keymap.set(mode, key, final_cmd, options)
+    else
+        -- If no message is provided, use cmd as is
+        vim.keymap.set(mode, key, cmd, options) -- cmd is used directly
+    end
 end
