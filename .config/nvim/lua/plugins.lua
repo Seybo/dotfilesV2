434e47028b9ac8c281007368db9565f376d5a171
seybo <michaelatworkspace@gmail.com>
2024-12-27 17:04:11 +0530

    Fix tab map for avante + add js bindings

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -477,14 +477,21 @@ local plugins = {
         event = "VeryLazy",
         lazy = false,
         version = false, -- set this if you want to always pull the latest change
+        opts = {
+            mappings = {
+                sidebar = {
+                    switch_windows = "<C-w>",
+                },
+            },
+        },
         build = "make",
         dependencies = {
             "stevearc/dressing.nvim",
             "nvim-lua/plenary.nvim",
             "MunifTanjim/nui.nvim",
             --- The below dependencies are optional,
-            "hrsh7th/nvim-cmp",
-            "nvim-tree/nvim-web-devicons",
+            "hrsh7th/nvim-cmp",            -- autocompletion for avante commands and mentions
+            "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
         },
     },
 
--- .config/nvim/lua/snippets/javascript.lua
+++ .config/nvim/lua/snippets/javascript.lua
@@ -2,6 +2,7 @@ local plugin = require "luasnip"
 local s = plugin.snippet
 local t = plugin.text_node
 local i = plugin.insert_node
+local r = require("luasnip.extras").rep
 
 return {
     s("bpp (debugger)", {
@@ -19,10 +20,17 @@ return {
         i(2, "message"),
         t("}`)"),
     }),
+    s("clgv (console.log)", {
+        t("console.log(`TODO_MM "),
+        i(1, "var"),
+        t(": ${JSON.stringify("),
+        r(1),
+        t(")}`)"),
+    }),
     s("tds (START_MM:)", {
         t("// START_MM: "),
         i(1, ""),
-        t({ "", "" }),         -- linebreak
+        t({ "", "" }), -- linebreak
         t("// ⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻"),
     }),
     s("tdd (TODO_MM:)", {
@@ -40,13 +48,13 @@ return {
     s("tdc (COMMENT_MM:)", {
         t("// COMMENT_MM: "),
         i(1, ""),
-        t({ "", "" }),         -- linebreak
+        t({ "", "" }), -- linebreak
         t("// ⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻"),
     }),
     s("tdq (QUESTION_MM:)", {
         t("// QUESTION_MM: "),
         i(1, ""),
-        t({ "", "" }),         -- linebreak
+        t({ "", "" }), -- linebreak
         t("// ⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻"),
     }),
 }
